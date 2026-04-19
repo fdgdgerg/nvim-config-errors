2441479d509feedc18afb8193723416e245c1c90
Joe Bu <joebu@Joes-MacBook-Pro-3.local>
2026-04-19 02:17:43 -0600

    conform disabled

---
+++ lua/test/plugins/conform.lua
@@ -0,0 +1,95 @@
+return {
+	"stevearc/conform.nvim",
+	enable = false,
+	event = { "BufReadPre", "BufNewFile" },
+	keys = {
+		{
+			"<leader>tf",
+			function()
+				-- If autoformat is currently disabled for this buffer,
+				-- then enable it, otherwise disable it
+				if vim.b.disable_autoformat then
+					vim.cmd("FormatEnable")
+					vim.notify("Enabled autoformat for current buffer")
+				else
+					vim.cmd("FormatDisable!")
+					vim.notify("Disabled autoformat for current buffer")
+				end
+			end,
+			desc = "Toggle autoformat for current buffer",
+		},
+		{
+			"<leader>tF",
+			function()
+				-- If autoformat is currently disabled globally,
+				-- then enable it globally, otherwise disable it globally
+				if vim.g.disable_autoformat then
+					vim.cmd("FormatEnable")
+					vim.notify("Enabled autoformat globally")
+				else
+					vim.cmd("FormatDisable")
+					vim.notify("Disabled autoformat globally")
+				end
+			end,
+			desc = "Toggle autoformat globally",
+		},
+	},
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				-- markdown = { "prettier" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				swift = { "swiftformat" },
+			},
+			format_on_save = function(bufnr)
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				local disable_filetypes = { c = false, cpp = false }
+				return {
+					timeout_ms = 1000,
+					async = false,
+					lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+				}
+			end,
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>sp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				-- :FormatDisable! disables autoformat for this buffer only
+				vim.b.disable_autoformat = true
+			else
+				-- :FormatDisable disables autoformat globally
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = "Disable autoformat-on-save",
+			bang = true, -- allows the ! variant
+		})
+
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Re-enable autoformat-on-save",
+		})
+	end,
+}
